db989d528087b200c5f5b8ca81f234c9f73b1745
Hassan Khan <johnhassankhan@gmail.com>
2018-03-13 21:41:58 +0500

    Insert and Update assignment

---
+++ index.js
@@ -0,0 +1,24 @@
+function set(){
+	var x=document.getElementById('search').value;
+	var z=document.getElementById('choice').value;
+	if(choice.value==1)
+		document.getElementById('first').innerHTML=x;
+	else if(choice.value==2)
+		document.getElementById('second').innerHTML=x;
+	else if(choice.value==3)
+		document.getElementById('third').innerHTML=x;
+	else if(choice.value==4)
+		document.getElementById('fourth').innerHTML=x;
+	else if(choice.value==5)
+		document.getElementById('fifth').innerHTML=x;
+	else if(choice.value==6)
+		document.getElementById('sixth').innerHTML=x;
+	else if(choice.value==7)
+		document.getElementById('seventh').innerHTML=x;
+	else if(choice.value==8)
+		document.getElementById('eight').innerHTML=x;
+	else if(choice.value==9)
+		document.getElementById('ninth').innerHTML=x;
+	else if(choice.value==10)
+		document.getElementById('tenth').innerHTML=x;
+}
